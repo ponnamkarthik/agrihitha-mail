80fb524fb188d957e391d10ee6ba2733feb6d854
Karthik Ponnam <ponnamkarthik3@gmail.com>
2023-05-16 12:32:47 +0530

    minor

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const axios = require('axios')
 const express = require('express')
+
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
@@ -13,11 +15,29 @@ app.post('/mail', (req, res) => {
     console.log(req.body)
     console.log("Just got a request!")
     res.send('Yo!')
+
+    let data = JSON.stringify(req.body);
+
+    let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.mailjet.com/v3.1/send',
+        headers: { 
+            'Content-Type': 'application/json', 
+            'Authorization': 'Basic ZjE5N2YwMTIwMTg1ZjM2MTRhMDI3ZjA3MDc1ZmM3N2E6OWYyMzE5NDQ0ZmMzNWI2M2EwYTc4OThiM2JhNmYzYjY='
+        },
+        data : data
+    };
+
+    axios.request(config)
+    .then((response) => {
+    console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+
 })
 
-app.post('/mail', (req, res) => {
-    console.log(req.body)
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+
 app.listen(process.env.PORT || 3000)
