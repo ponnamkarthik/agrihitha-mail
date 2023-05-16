e7a1d43cff5db5769c6aab38cd3a477a47aba597
Karthik Ponnam <ponnamkarthik3@gmail.com>
2023-05-16 12:29:51 +0530

    minor

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ app.all('/', (req, res) => {
     res.send('Yo!')
 })
 
+// get json from the request
+app.use(express.json())
+
+// create post endpoint for mail
+app.post('/mail', (req, res) => {
+    console.log(req.body)
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+
 app.post('/mail', (req, res) => {
     console.log(req.body)
     console.log("Just got a request!")
