a1bcaaa67ad71c2ffb5d88202740cac8e3886c51
Karthik Ponnam <ponnamkarthik3@gmail.com>
2023-05-16 12:26:29 +0530

    minor

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.post('/mail', (req, res) => {
+    console.log(req.body)
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
 app.listen(process.env.PORT || 3000)
